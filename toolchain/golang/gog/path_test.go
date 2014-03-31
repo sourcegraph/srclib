17bd8a6ab766c8cb93a9afaf2af4e045c3aa06db
Quinn Slack <qslack@qslack.com>
2014-03-30 22:14:42 -0700

    update gog tests

--- toolchain/golang/gog/path_test.go
+++ toolchain/golang/gog/path_test.go
@@ -18,17 +18,17 @@ func TestPaths(t *testing.T) {
 		{`type A struct { *B }; type B struct { c string }`, []defPath{{"foo", "A/B"}}, []defPath{{"foo", "A/B/c"}, {"foo", "A/c"}}},
 		{`func _() { var a int; _ = a }`, []defPath{{"foo", "_/a"}}, nil},
 		{`type A int; func (a A) x() { var b int; _ = b }`, []defPath{{"foo", "A/x/a"}, {"foo", "A/x/b"}}, nil},
-		{`func _() { if true { var a int; _ = a } }`, []defPath{{"foo", "_/$0/$0/a"}}, nil},
+		{`func _() { if true { var a int; _ = a } }`, []defPath{{"foo", "_/$sources[0]0/$sources[0]0/a"}}, nil},
 		{`type A int; func (a A) F() {}`, []defPath{{"foo", "A/F"}}, nil},
 		{`type A int; func (a *A) F() {}`, []defPath{{"foo", "A/F"}}, nil},
-		{`func F() {f := func(a int) (b int) { c := 7; return c; }; _ = f }`, []defPath{{"foo", "F/f"}, {"foo", "F/$0/a"}, {"foo", "F/$0/b"}, {"foo", "F/$0/c"}}, nil},
-		{`func F() { {a:=0;_=a};{a:=0;_=a} }`, []defPath{{"foo", "F/$0/a"}, {"foo", "F/$1/a"}}, nil},
-		{`func init() {}; func init() {}`, []defPath{{"foo", "init$28"}, {"foo", "init$44"}}, nil},
+		{`func F() {f := func(a int) (b int) { c := 7; return c; }; _ = f }`, []defPath{{"foo", "F/f"}, {"foo", "F/$sources[0]0/a"}, {"foo", "F/$sources[0]0/b"}, {"foo", "F/$sources[0]0/c"}}, nil},
+		{`func F() { {a:=0;_=a};{a:=0;_=a} }`, []defPath{{"foo", "F/$sources[0]0/a"}, {"foo", "F/$sources[0]1/a"}}, nil},
+		{`func init() {}; func init() {}`, []defPath{{"foo", "init$sources[0]28"}, {"foo", "init$sources[0]44"}}, nil},
 		{`var x struct { y int }`, []defPath{{"foo", "x/y"}}, nil},
 		{`func f(x struct{y int}) { _ = x.y }`, []defPath{{"foo", "f/x/y"}}, nil},
 		{`type I interface { A(); B() }`, []defPath{{"foo", "I"}, {"foo", "I/A"}, {"foo", "I/B"}}, nil},
 		{`type I interface { A(x int); B(x int) }`, []defPath{{"foo", "I/A/x"}, {"foo", "I/B/x"}}, nil},
-		{`type f func(i int); type g func(i int)`, []defPath{{"foo", "$sources[0]/$0/i"}, {"foo", "$sources[0]/$1/i"}}, nil},
+		{`type f func(i int); type g func(i int)`, []defPath{{"foo", "$sources[0]/$sources[0]0/i"}, {"foo", "$sources[0]/$sources[0]1/i"}}, nil},
 	}
 
 	for _, c := range cases {
