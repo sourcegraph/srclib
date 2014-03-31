02e48a3eaa72a1f17ad6975bf4480d6fa5ebbd48
Quinn Slack <qslack@qslack.com>
2014-03-30 21:55:24 -0700

    skip cached-config.json files in data file list

--- buildstore/store.go
+++ buildstore/store.go
@@ -107,6 +107,12 @@ func (s *RepositoryStore) ListCommits() ([]string, error) {
 	return commits, nil
 }
 
+const CachedRepositoryConfigFilename = "cached-config.json"
+
+var dataFilesIgnoreBasenames = map[string]struct{}{
+	CachedRepositoryConfigFilename: struct{}{},
+}
+
 func (s *RepositoryStore) DataFiles(path string) ([]*BuildDataFileInfo, error) {
 	var files []*BuildDataFileInfo
 	walker := fs.WalkFS(path, s.walkableRWVFS)
@@ -120,6 +126,11 @@ func (s *RepositoryStore) DataFiles(path string) ([]*BuildDataFileInfo, error) {
 		}
 
 		path := strings.TrimPrefix(walker.Path(), "/")
+
+		if _, ignore := dataFilesIgnoreBasenames[filepath.Base(path)]; ignore {
+			continue
+		}
+
 		parts := strings.SplitN(path, "/", 2)
 		if len(parts) != 2 {
 			return nil, fmt.Errorf("bad build data file path: %q", walker.Path())
