eed8d8cf8173695536ec2d94dec2c540f40bfe7c
Beyang Liu <beyang.liu@gmail.com>
2014-04-07 17:02:06 -0700

    better python dep

--- toolchain/python/python.go
+++ toolchain/python/python.go
@@ -3,5 +3,13 @@ package python
 import "sourcegraph.com/sourcegraph/srcgraph/toolchain"
 
 func init() {
-	toolchain.Register("python", struct{}{})
+	toolchain.Register("python", defaultPythonEnv)
+}
+
+type pythonEnv struct {
+	PythonVersion string
+}
+
+var defaultPythonEnv = &pythonEnv{
+	PythonVersion: "python2.7",
 }
