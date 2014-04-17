1d0b6b541f7c3cefd6814dd1215da3d36410c127
Beyang Liu <beyang.liu@gmail.com>
2014-04-16 22:07:58 -0700

    srcgraph make -test

--- make_cmd.go
+++ make_cmd.go
@@ -3,10 +3,12 @@ package srcgraph
 import (
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
-	"path/filepath"
+	"os/exec"
 
+	"strings"
 	"github.com/sourcegraph/go-vcs"
 	"github.com/sourcegraph/makex"
 	"sourcegraph.com/sourcegraph/srcgraph/build"
@@ -19,97 +21,180 @@ func makefile(args []string) {
 }
 
 func make_(args []string) {
+	params := mustParseMakeParams(args)
+	if err := params.verify(); err != nil {
+		log.Fatal(err)
+	}
+	repoConfig := params.RepositoryConfig.GetRepositoryConfig(task2.DefaultContext)
+
+	repoStore, err := buildstore.NewRepositoryStore(params.Repository.RootDir)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Get build directory (${REPO}/.sourcegraph-data/...)
+	var buildDir string
+	if params.Test {
+		var err error
+		buildDir, err = ioutil.TempDir("", fmt.Sprintf("sourcegraph-data.%s.%s-", strings.Replace(string(repoConfig.URI), "/", "-", -1),
+			params.Repository.CommitID))
+		if err != nil {
+			log.Fatal(err)
+		}
+		if params.TestKeep {
+			defer log.Printf("Test build directory: %s", buildDir)
+		} else {
+			defer os.RemoveAll(buildDir)
+		}
+	} else {
+		buildDir, err = buildstore.BuildDir(repoStore, params.Repository.CommitID)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	// Create Makefile
+	mf, err := build.CreateMakefile(buildDir, repoConfig)
+	if err != nil {
+		log.Fatalf("error creating Makefile: %s", err)
+	}
+
+	if *Verbose || params.ShowOnly {
+		// Show Makefile
+		data, err := makex.Marshal(mf)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(string(data))
+		if params.ShowOnly {
+			return
+		}
+	}
+
+	// Run Makefile
+	err = params.runMakefile(mf)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if params.Test {
+		// Compare expected with actual
+		expectedBuildDir, err := buildstore.BuildDir(repoStore, params.Repository.CommitID)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		diffOut, err := exec.Command("diff", "-ur", "--exclude=config.json", expectedBuildDir, buildDir).CombinedOutput()
+		log.Print("\n\n\n")
+		log.Print("###########################")
+		log.Print("##      TEST RESULTS     ##")
+		log.Print("###########################")
+		if len(diffOut) > 0 {
+			diffStr := string(diffOut)
+			diffStr = strings.Replace(diffStr, buildDir, "<test-build>", -1)
+			log.Printf(diffStr)
+			log.Printf("** FAIL **")
+		} else if err != nil {
+			log.Fatal(err)
+			log.Printf("** ERROR **")
+		} else if err == nil {
+			log.Printf("** PASS **")
+		}
+	}
+}
+
+type makeParams struct {
+	Repository       *repository
+	RepositoryConfig *repositoryConfigurator
+	Goals            []string
+
+	ShowOnly bool
+	Test     bool
+	TestKeep bool
+	Makex    *makex.Config
+}
+
+func mustParseMakeParams(args []string) *makeParams {
 	fs := flag.NewFlagSet("make", flag.ExitOnError)
 	r := AddRepositoryFlags(fs)
 	rc := AddRepositoryConfigFlags(fs, r)
-	showMakefileAndExit := fs.Bool("mf", false, "print generated Makefile and exit")
+	showOnly := fs.Bool("mf", false, "print generated Makefile and exit")
+	test := fs.Bool("test", false, "test build output against expected output in .sourcegraph-data/")
+	testKeep := fs.Bool("test-keep", false, "do NOT delete test build directory after test, used in conjunction with -test")
+
 	conf := &makex.Default
 	makex.Flags(fs, conf, "")
+
 	fs.Usage = func() {
 		fmt.Fprintln(os.Stderr, `usage: `+Name+` make [options] [target...]
 
-Generates and executes a Makefile that processes a repository, creating graph of
-definitions, references, and dependencies in a repository's code at a specific
-revision.
+	   Generates and executes a Makefile that processes a repository, creating graph of
+	   definitions, references, and dependencies in a repository's code at a specific
+	   revision.
 
-Run "`+Name+` makefile" to print the generated Makefile and exit.
+	   Run "`+Name+` makefile" to print the generated Makefile and exit.
 
-This command uses makex to execute the Makefile, but the Makefile is also
-compatible with GNU make. You can use the "`+Name+` makefile" command to
-generate a Makefile to use with GNU make, if you'd like.
+	   This command uses makex to execute the Makefile, but the Makefile is also
+	   compatible with GNU make. You can use the "`+Name+` makefile" command to
+	   generate a Makefile to use with GNU make, if you'd like.
 
-The options are:
-`)
+	   The options are:
+	   `)
 		fs.PrintDefaults()
 		fmt.Fprintln(os.Stderr)
 		os.Exit(1)
 	}
 	fs.Parse(args)
 
-	vcsType := vcs.VCSByName[r.vcsTypeName]
+	return &makeParams{
+		Repository:       r,
+		RepositoryConfig: rc,
+		Goals:            fs.Args(),
+		ShowOnly:         *showOnly,
+		Test:             *test,
+		TestKeep:         *testKeep,
+		Makex:            conf,
+	}
+}
+
+func (p *makeParams) verify() error {
+	vcsType := vcs.VCSByName[p.Repository.vcsTypeName]
 	if vcsType == nil {
-		log.Fatalf("%s: unknown VCS type %q", Name, r.vcsTypeName)
+		return fmt.Errorf("%s: unknown VCS type %q", Name, p.Repository.vcsTypeName)
 	}
+	return nil
+}
 
-	c := rc.GetRepositoryConfig(task2.DefaultContext)
-
-	// Get build data dir (${REPO}/.sourcegraph-data).
-	repoStore, err := buildstore.NewRepositoryStore(r.RootDir)
-	if err != nil {
-		log.Fatal(err)
-	}
-	rootDataDir, err := buildstore.RootDir(repoStore)
-	if err != nil {
-		log.Fatal(err)
-	}
-	buildDataDir, err := filepath.Rel(r.RootDir, filepath.Join(rootDataDir, repoStore.CommitPath(r.CommitID)))
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	mf, err := build.CreateMakefile(r.RootDir, r.CommitID, buildDataDir, c, conf, task2.DefaultContext)
-	if err != nil {
-		log.Fatalf("error creating Makefile: %s", err)
-	}
-
-	if *Verbose || *showMakefileAndExit {
-		data, err := makex.Marshal(mf)
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Println(string(data))
-		if *showMakefileAndExit {
-			return
-		}
-	}
-
-	goals := fs.Args()
+func (p *makeParams) runMakefile(mf *makex.Makefile) error {
+	goals := p.Goals
 	if len(goals) == 0 {
 		if defaultRule := mf.DefaultRule(); defaultRule != nil {
 			goals = []string{defaultRule.Target()}
 		} else {
-			log.Println("No rules in Makefile.")
-			return
+			// No rules in Makefile
+			return nil
 		}
 	}
 
-	mk := conf.NewMaker(mf, goals...)
+	mk := p.Makex.NewMaker(mf, goals...)
 
-	if conf.DryRun {
+	if p.Makex.DryRun {
 		err := mk.DryRun(os.Stdout)
 		if err != nil {
-			log.Fatal(err)
+			return err
 		}
-		return
+		return nil
 	}
 
-	err = os.Chdir(r.RootDir)
+	err := os.Chdir(p.Repository.RootDir)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 
 	err = mk.Run()
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
+
+	return nil
 }
