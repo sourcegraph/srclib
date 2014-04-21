e40126993cf3529fc7c3e715c45ea24bb10754fb
Quinn Slack <qslack@qslack.com>
2014-04-20 22:10:38 -0700

    rename

--- build/graph.go
+++ build/graph.go
@@ -20,23 +20,23 @@ func init() {
 func makeGraphRules(c *config.Repository, dataDir string, existing []makex.Rule) ([]makex.Rule, error) {
 	var rules []makex.Rule
 	for _, u := range c.SourceUnits {
-		rules = append(rules, &GraphSourceUnitRule{dataDir, u})
+		rules = append(rules, &GraphUnitRule{dataDir, u})
 	}
 	return rules, nil
 }
 
-type GraphSourceUnitRule struct {
+type GraphUnitRule struct {
 	dataDir string
 	Unit    unit.SourceUnit
 }
 
-func (r *GraphSourceUnitRule) Target() string {
+func (r *GraphUnitRule) Target() string {
 	return filepath.Join(r.dataDir, SourceUnitDataFilename(&grapher2.Output{}, r.Unit))
 }
 
-func (r *GraphSourceUnitRule) Prereqs() []string { return r.Unit.Paths() }
+func (r *GraphUnitRule) Prereqs() []string { return r.Unit.Paths() }
 
-func (r *GraphSourceUnitRule) Recipes() []string {
+func (r *GraphUnitRule) Recipes() []string {
 	return []string{
 		"mkdir -p `dirname $@`",
 		fmt.Sprintf("srcgraph -v graph -json %q 1> $@", unit.MakeID(r.Unit)),
