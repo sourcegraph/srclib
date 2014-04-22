da92de20c6a5dff091391c3f731f29535ec5de06
Beyang Liu <beyang.liu@gmail.com>
2014-04-21 23:44:17 -0700

    ruby dep

--- toolchain/ruby/dep.go
+++ toolchain/ruby/dep.go
@@ -1,14 +1,16 @@
 package ruby
 
 import (
+	"encoding/json"
+	"fmt"
+	"path/filepath"
+
 	"sourcegraph.com/sourcegraph/srcgraph/config"
 	"sourcegraph.com/sourcegraph/srcgraph/container"
 	"sourcegraph.com/sourcegraph/srcgraph/dep2"
 	"sourcegraph.com/sourcegraph/srcgraph/task2"
 	"sourcegraph.com/sourcegraph/srcgraph/unit"
 )
-
-const rubyGemTargetType = "ruby-gem"
 
 func init() {
 	dep2.RegisterLister(&gem{}, dep2.DockerLister{defaultRubyEnv})
@@ -17,26 +19,52 @@ func init() {
 }
 
 func (e *rubyEnv) BuildLister(dir string, unit unit.SourceUnit, c *config.Repository, x *task2.Context) (*container.Command, error) {
-
+	dockerfile, err := e.rdepDockerfile()
+	if err != nil {
+		return nil, err
+	}
 	return &container.Command{
 		Container: container.Container{
-			Dockerfile: e.DepDockerfile(),
+			Dockerfile: dockerfile,
 			RunOptions: []string{"-v", dir + ":" + srcRoot},
-			Cmd:        []string{"echo", "TODO"}, // TODO
+			Cmd:        []string{"rdep", filepath.Join(srcRoot, unit.RootDir())},
 			Stderr:     x.Stderr,
 			Stdout:     x.Stdout,
 		},
 		Transform: func(orig []byte) ([]byte, error) {
-			// TODO
-			return nil, nil
+			var metadata metadata_t
+			err := json.Unmarshal(orig, &metadata)
+			if err != nil {
+				return nil, err
+			}
+
+			deps := make([]*dep2.RawDependency, 0)
+			for _, dependency := range metadata.Dependencies {
+				if dependency.SourceURL != "" {
+					deps = append(deps, &dep2.RawDependency{
+						TargetType: rubyGemTargetType,
+						Target:     dependency,
+					})
+				}
+			}
+			return json.Marshal(deps)
 		},
 	}, nil
 }
 
 func (e *rubyEnv) Resolve(dep *dep2.RawDependency, c *config.Repository, x *task2.Context) (*dep2.ResolvedTarget, error) {
-	return nil, nil
-}
+	t, _ := json.Marshal(dep.Target)
+	var target dependency_t
+	json.Unmarshal(t, &target)
 
-func (e *rubyEnv) DepDockerfile() []byte {
-	return nil
+	switch dep.TargetType {
+	case rubyGemTargetType:
+		return &dep2.ResolvedTarget{
+			ToRepoCloneURL: target.SourceURL,
+			ToUnit:         target.Name,
+			ToUnitType:     unit.Type(&gem{}),
+		}, nil
+	default:
+		return nil, fmt.Errorf("Unexpected target type for Ruby: %+v", dep.TargetType)
+	}
 }
