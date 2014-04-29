20d3a5a69bed6b6794f2d86716706f8ad9e647b9
Quinn Slack <qslack@qslack.com>
2014-04-28 23:42:06 -0700

    longer names

--- graph/go_interface.go
+++ graph/go_interface.go
@@ -1,22 +1,20 @@
 package graph
 
-import (
-	"sourcegraph.com/sourcegraph/srcgraph/repo"
-)
+import "sourcegraph.com/sourcegraph/srcgraph/repo"
 
 // GoInterfaceMethod represents a Go interface method defined by an Go interface
 // symbol or implemented by a Go type symbol. It is used for finding all
 // implementations of an interface.
 type GoInterfaceMethod struct {
-	// Path refers to the Go interface symbol that defines this method, or the Go
+	// OfSymbolPath refers to the Go interface symbol that defines this method, or the Go
 	// type symbol that implements this method.
-	OfSymbol SymbolPath `db:"of_symbol"`
+	OfSymbolPath SymbolPath `db:"of_symbol_path"`
 
-	// OfUnit refers to the unit containing the symbol denoted in OfSymbol.
-	OfUnit string `db:"of_unit"`
+	// OfSymbolUnit refers to the unit containing the symbol denoted in OfSymbolPath.
+	OfSymbolUnit string `db:"of_symbol_unit"`
 
 	// Repo refers to the repository in which this method was defined.
-	Repo repo.URI 
+	Repo repo.URI
 
 	// Key is the canonical signature of the method for the implements
 	// operation. If a type's methods' keys are a superset of an interface's,
@@ -24,5 +22,5 @@ type GoInterfaceMethod struct {
 	CanonicalSignature string `db:"canonical_signature"`
 
 	// Name is the method's name.
-	Name string 
+	Name string
 }
