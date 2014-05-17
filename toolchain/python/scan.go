0b1a4586bf1d1ff223d2d1af5404371e256df437
Quinn Slack <qslack@qslack.com>
2014-05-17 03:21:42 -0700

    require python source units to have a setup.py file (fixes #766)

--- toolchain/python/scan.go
+++ toolchain/python/scan.go
@@ -19,14 +19,20 @@ type fauxScanner struct{}
 func (p *fauxScanner) Scan(dir string, c *config.Repository) ([]unit.SourceUnit, error) {
 	var files []string
 	walker := fs.Walk(dir)
+	var foundSetupPy bool
 	for walker.Step() {
 		if err := walker.Err(); err == nil && !walker.Stat().IsDir() && filepath.Ext(walker.Path()) == ".py" {
+			// pydep will fail if there's no setup.py file.
+			if filepath.Base(walker.Path()) == "setup.py" {
+				foundSetupPy = true
+			}
+
 			file, _ := filepath.Rel(dir, walker.Path())
 			files = append(files, file)
 		}
 	}
 
-	if len(files) > 0 {
+	if len(files) > 0 && foundSetupPy {
 		return []unit.SourceUnit{&FauxPackage{Files: files}}, nil
 	} else {
 		return nil, nil
