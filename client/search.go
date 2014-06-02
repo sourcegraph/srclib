22f02983aad7fc9b93184eed0852d810d7a872ac
Quinn Slack <qslack@qslack.com>
2014-06-01 18:07:40 -0700

    basic search

---
+++ client/search.go
@@ -0,0 +1,65 @@
+package client
+
+import (
+	"sourcegraph.com/sourcegraph/api_router"
+	"sourcegraph.com/sourcegraph/srcgraph/person"
+)
+
+// SearchService communicates with the search-related endpoints in
+// the Sourcegraph API.
+type SearchService interface {
+	// Search searches the full index.
+	Search(opt *SearchOptions) (*SearchResults, Response, error)
+}
+
+type SearchResults struct {
+	Symbols      []*Symbol
+	People       []*person.User
+	Repositories []*Repository
+}
+
+// searchService implements SearchService.
+type searchService struct {
+	client *Client
+}
+
+var _ SearchService = &searchService{}
+
+type SearchOptions struct {
+	Query string `url:"q" schema:"q"`
+
+	ListOptions
+}
+
+func (s *searchService) Search(opt *SearchOptions) (*SearchResults, Response, error) {
+	url, err := s.client.url(api_router.Search, nil, opt)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	req, err := s.client.NewRequest("GET", url.String(), nil)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	var results *SearchResults
+	resp, err := s.client.Do(req, &results)
+	if err != nil {
+		return nil, resp, err
+	}
+
+	return results, resp, nil
+}
+
+type MockSearchService struct {
+	Search_ func(opt *SearchOptions) (*SearchResults, Response, error)
+}
+
+var _ SearchService = MockSearchService{}
+
+func (s MockSearchService) Search(opt *SearchOptions) (*SearchResults, Response, error) {
+	if s.Search_ == nil {
+		return nil, &HTTPResponse{}, nil
+	}
+	return s.Search_(opt)
+}
