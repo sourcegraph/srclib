1ebb72f8223ea1579d75666f1bf65a73f6e224ab
Quinn Slack <qslack@qslack.com>
2014-06-27 10:59:19 -0700

    Test for `srcgraph make` that uses git submodules and is easier to update

---
+++ make_test.go
@@ -0,0 +1,151 @@
+package srcgraph
+
+import (
+	"bytes"
+	"flag"
+	"io"
+	"io/ioutil"
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+
+	"sourcegraph.com/sourcegraph/srcgraph/buildstore"
+
+	"testing"
+
+	"github.com/aybabtme/color/brush"
+)
+
+var mode = flag.String("test.mode", "test", "[test|keep|gen] 'test' runs test as normal; keep keeps around generated test files for inspection after tests complete; 'gen' generates new expected test data")
+var repoMatch = flag.String("test.repo", "", "only test `srcgraph make` for repos that contain this string")
+
+func TestMakeCmd(t *testing.T) {
+	// Since we exec `srcgraph`, make sure it's up-to-date.
+	if out, err := exec.Command("make", "-C", "..", "srcgraph").CombinedOutput(); err != nil {
+		t.Errorf("Failed to build srcgraph for `srcgraph make` tests: %s.\n\nOutput was:\n%s", err, out)
+		return
+	}
+
+	const testReposDir = "testdata/repos"
+	fis, err := ioutil.ReadDir("testdata/repos")
+	if err != nil {
+		t.Fatal(err)
+	}
+	n := 0
+	for _, fi := range fis {
+		if repoDir := fi.Name(); strings.Contains(repoDir, *repoMatch) {
+			testMakeCmd(t, filepath.Join(testReposDir, repoDir))
+			n++
+		}
+	}
+	if n == 0 {
+		t.Errorf("No TestMakeCmd cases were run. Did none match your constraint -test.match=%q?", *repoMatch)
+	}
+}
+
+func testMakeCmd(t *testing.T, repoDir string) {
+	// Update the git submodule automatically.
+	cmd := exec.Command("git", "submodule", "update", filepath.Join("srcgraph", repoDir))
+	cmd.Dir = ".."
+	if out, err := cmd.CombinedOutput(); err != nil {
+		t.Errorf("Failed to update git submodule for %s: %s.\n\nOutput was:\n%s", repoDir, err, out)
+		return
+	}
+
+	repoName := filepath.Base(repoDir)
+
+	// Directories for the actual ("got") and expected ("want") outputs.
+	gotOutputDir := filepath.Join(repoDir, "../../repos-output/got", repoName)
+	wantOutputDir := filepath.Join(repoDir, "../../repos-output/want", repoName)
+
+	// Determine and wipe the desired output dir.
+	var outputDir string
+	if *mode == "gen" {
+		outputDir = wantOutputDir
+	} else {
+		outputDir = gotOutputDir
+	}
+	outputDir, _ = filepath.Abs(outputDir)
+	if err := os.RemoveAll(outputDir); err != nil {
+		t.Fatal(err)
+	}
+	if err := os.MkdirAll(outputDir, 0755); err != nil {
+		t.Fatal(err)
+	}
+
+	// Symlink ${repoDir}/.sourcegraph-data/${commitID} to the desired output dir.
+	origOutputDestDir := filepath.Join(repoDir, buildstore.BuildDataDirName, getHEADOrTipCommitID(t, repoDir))
+	if err := os.Remove(origOutputDestDir); err != nil && !os.IsNotExist(err) {
+		t.Fatal(err)
+	}
+	if err := os.Symlink(outputDir, origOutputDestDir); err != nil {
+		t.Error(err)
+		return
+	}
+	defer func() {
+		// Remove the symlink when we're done so the repo doesn't have
+		// uncommitted changes.
+		if err := os.Remove(origOutputDestDir); err != nil {
+			t.Fatal(err)
+		}
+	}()
+
+	// Run `srcgraph make`.
+	var buf bytes.Buffer
+	w := io.MultiWriter(&buf, os.Stderr)
+	cmd = exec.Command("srcgraph", "-v", "make")
+	cmd.Stderr, cmd.Stdout = w, w
+	cmd.Dir = repoDir
+
+	err := cmd.Run()
+	if err != nil {
+		t.Errorf("Command %v in %s failed: %s.\n\nOutput was:\n%s", cmd.Args, repoDir, err, buf.String())
+		return
+	}
+
+	if *mode == "gen" {
+		t.Errorf("Successfully generated expected output for %s in %s. (Triggering test error so you won't mistakenly interpret a 0 return code as a test success. Run without -test.mode=gen to run the test.)", repoName, wantOutputDir)
+	} else {
+		checkResults(t, repoName, gotOutputDir, wantOutputDir)
+	}
+}
+
+func checkResults(t *testing.T, repoName, gotOutputDir, wantOutputDir string) {
+	out, err := exec.Command("diff", "-ur", wantOutputDir, gotOutputDir).CombinedOutput()
+	if err != nil || len(out) > 0 {
+		t.Errorf("Diff failed for %s: %s.", repoName, err)
+		if len(out) > 0 {
+			t.Log(string(colorizeDiff(out)))
+			t.Errorf("Output for %s differed from expected.", repoName)
+		}
+	} else {
+		t.Logf(brush.Green(repoName + " PASS").String())
+	}
+}
+
+func getHEADOrTipCommitID(t *testing.T, repoDir string) string {
+	// TODO(sqs): assumes git
+	cmd := exec.Command("git", "rev-parse", "HEAD")
+	cmd.Dir = repoDir
+	out, err := cmd.Output()
+	if err != nil {
+		t.Fatal(err)
+	}
+	return string(bytes.TrimSpace(out))
+}
+
+func colorizeDiff(diff []byte) []byte {
+	lines := bytes.Split(diff, []byte{'\n'})
+	for i, line := range lines {
+		log.Printf("XXXXXX %q", line)
+		if bytes.HasPrefix(line, []byte{'-'}) {
+			lines[i] = []byte(brush.Red(string(line)).String())
+		}
+		if bytes.HasPrefix(line, []byte{'+'}) {
+			lines[i] = []byte(brush.Green(string(line)).String())
+		}
+	}
+	return bytes.Join(lines, []byte{'\n'})
+}
