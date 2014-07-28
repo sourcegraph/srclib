00c5b628e91431dcc7abe6d102742022086db551
Quinn Slack <qslack@qslack.com>
2014-07-28 16:18:37 -0700

    choose tool to use for a given task based on available tools in all available toolchains (toolchain.ChooseTool)

---
+++ toolchain/choose_test.go
@@ -0,0 +1,33 @@
+package toolchain
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestChooseTool(t *testing.T) {
+	// TODO(sqs): implement this test
+	tests := []struct {
+		toolchains   []*Info
+		op, unitType string
+		want         *ToolRef
+		wantErr      error
+	}{{}}
+	for _, test := range tests {
+		tool, err := chooseTool(test.op, test.unitType, test.toolchains)
+		if err != nil {
+			if test.wantErr == nil {
+				t.Errorf("got error %q, want no error", err)
+				continue
+			}
+			if test.wantErr != nil && err.Error() != test.wantErr.Error() {
+				t.Errorf("got error %q, want %q", err, test.wantErr)
+				continue
+			}
+		}
+
+		if !reflect.DeepEqual(tool, test.want) {
+			t.Errorf("got tool %+v, want %+v", tool, test.want)
+		}
+	}
+}
