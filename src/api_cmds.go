fe93d6413c7bab8e507cb62fe88cdab1d1a382ce
Quinn Slack <qslack@qslack.com>
2014-07-29 09:59:32 -0700

    `src api describe` editor plugin API cmd

--- src/api_cmds.go
+++ src/api_cmds.go
@@ -1,8 +1,19 @@
 package src
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/kr/fs"
+	"github.com/sourcegraph/srclib/buildstore"
+	"github.com/sourcegraph/srclib/graph"
+	"github.com/sourcegraph/srclib/grapher"
+	"github.com/sourcegraph/srclib/plan"
+	"github.com/sourcegraph/srclib/unit"
 )
 
 func init() {
@@ -39,7 +50,98 @@ type APIDescribeCmd struct {
 var apiDescribeCmd APIDescribeCmd
 
 func (c *APIDescribeCmd) Execute(args []string) error {
-	// TODO(sqs): implement these for real
-	fmt.Println(`{"Name":"myname","Doc":"mydoc"}\n`)
+	repo, err := OpenRepo(filepath.Dir(c.File))
+	if err != nil {
+		return err
+	}
+
+	c.File, err = filepath.Rel(repo.RootDir, c.File)
+	if err != nil {
+		return err
+	}
+
+	buildStore, err := buildstore.NewRepositoryStore(repo.RootDir)
+	if err != nil {
+		return err
+	}
+
+	// TODO(sqs): This whole lookup is totally inefficient. The storage format
+	// is not optimized for lookups.
+
+	// Find all source unit definition files.
+	var unitFiles []string
+	unitSuffix := buildstore.DataTypeSuffix(unit.SourceUnit{})
+	w := fs.WalkFS(buildStore.CommitPath(repo.CommitID), buildStore)
+	for w.Step() {
+		if strings.HasSuffix(w.Path(), unitSuffix) {
+			unitFiles = append(unitFiles, w.Path())
+		}
+	}
+
+	// Find which source units the file belongs to.
+	var units []*unit.SourceUnit
+	for _, unitFile := range unitFiles {
+		var u *unit.SourceUnit
+		f, err := buildStore.Open(unitFile)
+		if err != nil {
+			return err
+		}
+		defer f.Close()
+		if err := json.NewDecoder(f).Decode(&u); err != nil {
+			return fmt.Errorf("%s: %s", unitFile, err)
+		}
+		for _, f2 := range u.Files {
+			if f2 == c.File {
+				units = append(units, u)
+				break
+			}
+		}
+	}
+
+	if len(units) > 0 {
+		ids := make([]string, len(units))
+		for i, u := range units {
+			ids[i] = string(u.ID())
+		}
+		log.Printf("Position %s:%d is in %d source units %v.", c.File, c.StartByte, len(units), ids)
+	} else {
+		log.Printf("Position %s:%d is not in any source units.", c.File, c.StartByte)
+		return nil
+	}
+
+	// Find the ref(s) at the character position.
+	var ref *graph.Ref
+OuterLoop:
+	for _, u := range units {
+		var g grapher.Output
+		graphFile := buildStore.FilePath(repo.CommitID, plan.SourceUnitDataFilename("graph", u))
+		f, err := buildStore.Open(graphFile)
+		if err != nil {
+			return err
+		}
+		defer f.Close()
+		if err := json.NewDecoder(f).Decode(&g); err != nil {
+			return fmt.Errorf("%s: %s", graphFile, err)
+		}
+		for _, ref2 := range g.Refs {
+			if c.File == ref2.File && c.StartByte >= ref2.Start && c.StartByte <= ref2.End {
+				ref = ref2
+				break OuterLoop
+			}
+		}
+	}
+
+	if ref == nil {
+		log.Printf("No ref found at %s:%d.", c.File, c.StartByte)
+		return nil
+	}
+
+	if ref.SymbolRepo == "" {
+		ref.SymbolRepo = repo.URI()
+	}
+
+	if err := json.NewEncoder(os.Stdout).Encode(ref); err != nil {
+		return err
+	}
 	return nil
 }
