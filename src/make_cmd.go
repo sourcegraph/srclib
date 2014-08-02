e567ee33c2d3345c8ebc85e62b80c57ff697cf82
Quinn Slack <qslack@qslack.com>
2014-08-02 11:11:36 -0700

    factor out CreateMaker

--- src/make_cmd.go
+++ src/make_cmd.go
@@ -40,7 +40,7 @@ type MakeCmd struct {
 	Dir Directory `short:"C" long:"directory" description:"change to DIR before doing anything" value-name:"DIR"`
 
 	Args struct {
-		Targets []string `name:"TARGETS..." description:"Makefile targets to build (default: all)"`
+		Goals []string `name:"GOALS..." description:"Makefile targets to build (default: all)"`
 	} `positional-args:"yes"`
 }
 
@@ -53,44 +53,12 @@ func (c *MakeCmd) Execute(args []string) error {
 		}
 	}
 
-	if len(c.Args.Targets) == 0 {
-		c.Args.Targets = []string{"all"}
-	}
-
-	currentRepo, err := OpenRepo(".")
+	mk, mf, err := CreateMaker(c.ToolchainExecOpt, c.Args.Goals)
 	if err != nil {
 		return err
 	}
-	buildStore, err := buildstore.NewRepositoryStore(currentRepo.RootDir)
-	if err != nil {
-		return err
-	}
-
-	treeConfig, err := config.ReadCached(buildStore, currentRepo.CommitID)
-	if err != nil {
-		return err
-	}
-
-	toolchainExecOptArgs, err := toolchain.MarshalArgs(&c.ToolchainExecOpt)
-	if err != nil {
-		return err
-	}
-
-	buildDataDir, err := buildstore.BuildDir(buildStore, currentRepo.CommitID)
-	if err != nil {
-		return err
-	}
-	absDir, err := os.Getwd()
-	if err != nil {
-		return err
-	}
-	buildDataDir, _ = filepath.Rel(absDir, buildDataDir)
 
 	if c.PrintMakefile {
-		mf, err := plan.CreateMakefile(buildDataDir, treeConfig, plan.Options{strings.Join(toolchainExecOptArgs, " ")})
-		if err != nil {
-			return err
-		}
 		mfData, err := makex.Marshal(mf)
 		if err != nil {
 			return err
@@ -99,17 +67,59 @@ func (c *MakeCmd) Execute(args []string) error {
 		return err
 	}
 
-	mf, err := plan.CreateMakefile(buildDataDir, treeConfig, plan.Options{strings.Join(toolchainExecOptArgs, " ")})
-	if err != nil {
-		return err
-	}
-
-	mkConf := &makex.Default
-	mk := mkConf.NewMaker(mf, c.Args.Targets...)
-
 	if c.DryRun {
 		return mk.DryRun(os.Stdout)
 	}
 
 	return mk.Run()
+}
+
+// CreateMaker creates a Makefile and a Maker. The cwd should be the root of the
+// tree you want to make (due to some probably unnecessary assumptions that
+// CreateMaker makes).
+func CreateMaker(execOpt ToolchainExecOpt, goals []string) (*makex.Maker, *makex.Makefile, error) {
+	currentRepo, err := OpenRepo(".")
+	if err != nil {
+		return nil, nil, err
+	}
+	buildStore, err := buildstore.NewRepositoryStore(currentRepo.RootDir)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	treeConfig, err := config.ReadCached(buildStore, currentRepo.CommitID)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	toolchainExecOptArgs, err := toolchain.MarshalArgs(&execOpt)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	buildDataDir, err := buildstore.BuildDir(buildStore, currentRepo.CommitID)
+	if err != nil {
+		return nil, nil, err
+	}
+	absDir, err := os.Getwd()
+	if err != nil {
+		return nil, nil, err
+	}
+	buildDataDir, _ = filepath.Rel(absDir, buildDataDir)
+
+	mf, err := plan.CreateMakefile(buildDataDir, treeConfig, plan.Options{strings.Join(toolchainExecOptArgs, " ")})
+	if err != nil {
+		return nil, nil, err
+	}
+
+	if len(goals) == 0 {
+		if defaultRule := mf.DefaultRule(); defaultRule != nil {
+			goals = []string{defaultRule.Target()}
+		}
+	}
+
+	mkConf := &makex.Default
+	mk := mkConf.NewMaker(mf, goals...)
+
+	return mk, mf, nil
 }
