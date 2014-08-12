3e1c7d32b85b4769af71598e361115a1c2c9cdb5
Quinn Slack <qslack@qslack.com>
2014-08-12 01:44:04 -0700

    nodejs stdlib override

--- config/overrides.go
+++ config/overrides.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"sourcegraph.com/sourcegraph/srclib/repo"
+	"sourcegraph.com/sourcegraph/srclib/unit"
 )
 
 var overrides = map[repo.URI]*Repository{
@@ -11,6 +12,32 @@ var overrides = map[repo.URI]*Repository{
 			Config:            map[string]interface{}{"GOROOT": "."},
 			SkipDirs:          []string{"test", "misc", "doc", "lib", "include"},
 			PreConfigCommands: []string{"cd src && ./make.bash"},
+		},
+	},
+	"github.com/joyent/node": {
+		URI: "github.com/joyent/node",
+		Tree: Tree{
+			SkipDirs: []string{"tools", "deps", "test", "src"},
+			SourceUnits: []*unit.SourceUnit{
+				{
+					Name:  "node",
+					Type:  "CommonJSPackage",
+					Dir:   ".",
+					Files: []string{"lib/*.js"},
+					Config: map[string]interface{}{
+						"jsg": map[string]interface{}{
+							"plugins": map[string]interface{}{
+								"node": map[string]string{"coreModulesDir": "lib/"},
+								"$(JSG_DIR)/node_modules/tern-node-api-doc/node-api-doc": map[string]string{
+									"apiDocDir":      "doc/api/",
+									"apiSrcDir":      "lib/",
+									"generateJSPath": "tools/doc/generate.js",
+								},
+							},
+						},
+					},
+				},
+			},
 		},
 	},
 }
