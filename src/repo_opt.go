737e0ed8a9ea48d2262db73bb8af4a229706c888
Quinn Slack <qslack@qslack.com>
2014-08-15 01:31:14 -0700

    Don't complain if not run in a repo dir, and only check repo once

--- src/repo_opt.go
+++ src/repo_opt.go
@@ -7,18 +7,27 @@ import (
 	"github.com/sqs/go-flags"
 )
 
+var (
+	currentRepo    *Repo
+	currentRepoErr error
+)
+
 func SetRepoOptDefaults(c *flags.Command) {
-	currentRepo, err := OpenRepo(".")
-	if err != nil {
-		log.Println(err)
-		return
+	// only try to open the current-dir repo once (we'd get the same result each
+	// time, since we never modify it)
+	if currentRepo == nil && currentRepoErr == nil {
+		currentRepo, currentRepoErr = OpenRepo(".")
 	}
 
-	SetOptionDefaultValue(c.Group, "repo", string(currentRepo.URI()))
+	if currentRepo != nil {
+		if currentRepo.CloneURL != "" {
+			SetOptionDefaultValue(c.Group, "repo", string(currentRepo.URI()))
+		}
 
-	subdir, err := filepath.Rel(currentRepo.RootDir, absDir)
-	if err != nil {
-		log.Fatal(err)
+		subdir, err := filepath.Rel(currentRepo.RootDir, absDir)
+		if err != nil {
+			log.Fatal(err)
+		}
+		SetOptionDefaultValue(c.Group, "subdir", subdir)
 	}
-	SetOptionDefaultValue(c.Group, "subdir", subdir)
 }
