639be64dcc2e64c454d75ddf21912128340ded85
Beyang Liu <beyang@sourcegraph.com>
2014-09-25 16:01:29 -0700

    add TryMakeURI

--- repo/uri.go
+++ repo/uri.go
@@ -2,6 +2,7 @@ package repo
 
 import (
 	"database/sql/driver"
+	"errors"
 	"fmt"
 	"net/url"
 	"path/filepath"
@@ -31,19 +32,27 @@ func (u URI) GitHubURL() string {
 // "git://github.com/user/repo.git", to a normalized URI string, such as
 // "github.com/user/repo".
 func MakeURI(cloneURL string) URI {
+	uri, err := TryMakeURI(cloneURL)
+	if err != nil {
+		panic(err)
+	}
+	return uri
+}
+
+func TryMakeURI(cloneURL string) (URI, error) {
 	if cloneURL == "" {
-		panic("MakeURI: empty clone URL")
+		return "", errors.New("MakeURI: empty clone URL")
 	}
 
 	url, err := url.Parse(cloneURL)
 	if err != nil {
-		panic(fmt.Sprintf("MakeURI(%q): %s", cloneURL, err))
+		return "", fmt.Errorf("MakeURI(%q): %s", cloneURL, err)
 	}
 
 	path := strings.TrimSuffix(url.Path, ".git")
 	path = filepath.Clean(path)
 	path = strings.TrimSuffix(path, "/")
-	return URI(strings.ToLower(url.Host) + path)
+	return URI(strings.ToLower(url.Host) + path), nil
 }
 
 // URIEqual returns true if a and b are equal, based on a case insensitive
