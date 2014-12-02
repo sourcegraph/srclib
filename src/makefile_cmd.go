5584bdb5dcd9384bb4d148fb46ff17413834ff62
Quinn Slack <qslack@qslack.com>
2014-12-02 15:41:25 -0800

    separate `src make` and `src makefile` commands

---
+++ src/makefile_cmd.go
@@ -0,0 +1,39 @@
+package src
+
+import (
+	"log"
+	"os"
+
+	"sourcegraph.com/sourcegraph/makex"
+)
+
+func init() {
+	_, err := CLI.AddCommand("makefile",
+		"prints the Makefile that `src make` executes",
+		"The makefile command prints the Makefile that `src make` will execute.",
+		&makefileCmd,
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+type MakefileCmd struct {
+	ToolchainExecOpt `group:"execution"`
+}
+
+var makefileCmd MakefileCmd
+
+func (c *MakefileCmd) Execute(args []string) error {
+	mf, err := CreateMakefile(c.ToolchainExecOpt)
+	if err != nil {
+		return err
+	}
+
+	mfData, err := makex.Marshal(mf)
+	if err != nil {
+		return err
+	}
+	_, err = os.Stdout.Write(mfData)
+	return err
+}
