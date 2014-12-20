e41d70ca4545650eb65a178f142a4bfda9acd418
Samer Masterson <samer@samertm.com>
2014-12-20 04:49:20 -0800

    symlink unit files

--- src/config_cmd.go
+++ src/config_cmd.go
@@ -23,7 +23,7 @@ import (
 func init() {
 	c, err := CLI.AddCommand("config",
 		"reads & scans for project configuration",
-		`Produces a configuration file suitable for building the repository or directory tree rooted at DIR (or the current directory if not specified).
+		`meowProduces a configuration file suitable for building the repository or directory tree rooted at DIR (or the current directory if not specified).
 
 The steps are:
 
@@ -142,14 +142,52 @@ func (c *ConfigCmd) Execute(args []string) error {
 	// or maybe a custom stale checker is better than just using file mtimes for
 	// all the files (maybe just use setup.py as a prereq? but then how will we
 	// update SourceUnit.Files list? SourceUnit.Globs could help here...)
+	//
+	// note(samertm): going with a custom checker because we need
+	// to create a symlink to the previous unit/graph files if a
+	// source unit hasn't changed.
 	if !c.NoCacheWrite {
 		if err := rwvfs.MkdirAll(commitFS, "."); err != nil {
 			return err
 		}
+		// Check to see if a previous build exists.
+		var prevRev string
+		var changedFiles []string
+		if revs, err := listRevisions(currentRepo.VCSType, buildstore.BuildDataDirName); err != nil {
+			log.Println("error listing revisions, rebuilding from scratch: %s", err)
+		} else {
+			// Skip HEAD, the first revision in the list.
+			for i := 1; i < len(revs); i++ {
+				if !buildStore.Exists(revs[i]) {
+					continue
+				}
+				// A build store exists for this commit. Now we need
+				// to get all the changed files between this rev and
+				// the current rev.
+				files, err := changedFilesFromCurrentRev(currentRepo.VCSType, buildstore.BuildDataDirName, revs[i])
+				if err != nil {
+					log.Println("error retriving changed files, rebuilding from scratch: %s", err)
+					break
+				}
+				changedFiles = files
+				prevRev = revs[i]
+			}
+		}
+		log.Println("FOUND", changedFiles, prevRev)
 		for _, u := range cfg.SourceUnits {
 			unitFile := plan.SourceUnitDataFilename(unit.SourceUnit{}, u)
 			if err := rwvfs.MkdirAll(commitFS, filepath.Dir(unitFile)); err != nil {
 				return err
+			}
+			if prevRev != "" && !u.ContainsAny(changedFiles) {
+				log.Println("SYMLINKING", u)
+				// make symlink
+				if err := buildStore.Symlink(
+					filepath.Join(prevRev, unitFile),
+					filepath.Join(currentRepo.CommitID, unitFile)); err != nil {
+					return err
+				}
+				continue
 			}
 			f, err := commitFS.Create(unitFile)
 			if err != nil {
