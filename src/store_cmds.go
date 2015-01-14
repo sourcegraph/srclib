330bc50d2b3efe9f997ef295183e00a39a3e6ae3
Samer Masterson <samer@samertm.com>
2015-01-14 02:23:31 -0800

    add import command back in!

--- src/store_cmds.go
+++ src/store_cmds.go
@@ -6,6 +6,7 @@ import (
 	"os"
 
 	"sourcegraph.com/sourcegraph/srclib"
+	"sourcegraph.com/sourcegraph/srclib/buildstore"
 	"sourcegraph.com/sourcegraph/srclib/config"
 	"sourcegraph.com/sourcegraph/srclib/graph"
 	"sourcegraph.com/sourcegraph/srclib/graphstore"
@@ -16,6 +17,15 @@ func init() {
 		"graph store commands",
 		"",
 		&storeCmd,
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = c.AddCommand("import",
+		"import data",
+		`Import data into the graph store. Only imports ref data for now.`,
+		&storeImportCmd,
 	)
 	if err != nil {
 		log.Fatal(err)
@@ -41,13 +51,47 @@ func init() {
 	}
 }
 
-type StoreCmd struct {
-	config.Options
-}
+type StoreCmd struct{}
 
 var storeCmd StoreCmd
 
 func (c *StoreCmd) Execute(args []string) error { return nil }
+
+type StoreImportCmd struct {
+	config.Options
+}
+
+var storeImportCmd StoreImportCmd
+
+func (c *StoreImportCmd) Execute(args []string) error {
+	// TODO(samer): honor options.
+	localRepo, err := OpenRepo(".")
+	if err != nil {
+		return err
+	}
+	buildStore, err := buildstore.LocalRepo(localRepo.RootDir)
+	if err != nil {
+		return err
+	}
+	output, err := config.ReadCachedGraph(buildStore.Commit(localRepo.CommitID))
+	if err != nil {
+		return err
+	}
+	if len(output.Refs) == 0 {
+		log.Println("No refs found.")
+		// This is not an error, but there is no more work to
+		// be done.
+		return nil
+	}
+	gs, err := graphstore.NewLocal(srclib.Path)
+	if err != nil {
+		return err
+	}
+	if err := gs.StoreRefs(output.Refs); err != nil {
+		return err
+	}
+	return nil
+}
 
 type ListRefsCmd struct {
 	Repo     string `long:"repo" required:"true"`
