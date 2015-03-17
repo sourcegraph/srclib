b90a8627144356bb4445a789bfc5018bdc668cb4
Samer Masterson <samer@samertm.com>
2015-03-17 15:12:34 -0700

    remove updateVCSIgnore
    Writing to .gitignore was failing homebrew builds.

--- src/util.go
+++ src/util.go
@@ -1,25 +1,20 @@
 package src
 
 import (
-	"bytes"
 	"encoding/json"
 	"fmt"
 	"io"
-	"io/ioutil"
 	"log"
 	"math"
 	"net/http"
 	"net/http/httputil"
 	"os"
 	"os/exec"
-	"path/filepath"
 	"strings"
 
 	"golang.org/x/tools/godoc/vfs"
 
-	"sourcegraph.com/sourcegraph/srclib/buildstore"
 	"sourcegraph.com/sourcegraph/srclib/unit"
-	"sourcegraph.com/sourcegraph/srclib/util"
 )
 
 func isDir(dir string) bool {
@@ -104,29 +99,6 @@ func OpenInputFiles(extraArgs []string) map[string]io.ReadCloser {
 func CloseAll(files map[string]io.ReadCloser) {
 	for _, rc := range files {
 		rc.Close()
-	}
-}
-
-// updateVCSIgnore adds .srclib-cache/ to the user's .${VCS}ignore file in
-// their home directory.
-func updateVCSIgnore(name string) {
-	homeDir := util.CurrentUserHomeDir()
-
-	entry := buildstore.BuildDataDirName + "/"
-
-	path := filepath.Join(homeDir, name)
-	data, err := ioutil.ReadFile(path)
-	if os.IsNotExist(err) {
-		err = nil
-	} else if bytes.Contains(data, []byte("\n"+entry+"\n")) {
-		// already has entry
-		return
-	}
-
-	data = append(data, []byte("\n\n# srclib build cache\n"+entry+"\n")...)
-	err = ioutil.WriteFile(path, data, 0700)
-	if err != nil {
-		log.Fatal(err)
 	}
 }
 
