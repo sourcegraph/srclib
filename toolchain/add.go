ebd7758266d91b1b74456c9a07d2c7b3533085b8
Samer Masterson <samer@samertm.com>
2015-04-02 16:16:25 -0700

    Use the force, toolchain add.
    Add "force" option to toolchain add.

--- toolchain/add.go
+++ toolchain/add.go
@@ -9,11 +9,21 @@ import (
 	"sourcegraph.com/sourcegraph/srclib"
 )
 
+type AddOpt struct {
+	// Force add a toolchain, overwriting any existing toolchains.
+	Force bool
+}
+
 // Add creates a symlink in the SRCLIBPATH so that the toolchain in dir is
 // available at the toolchainPath.
-func Add(dir, toolchainPath string) error {
-	if _, err := Lookup(toolchainPath); !os.IsNotExist(err) {
-		return fmt.Errorf("a toolchain already exists at toolchain path %q", toolchainPath)
+func Add(dir, toolchainPath string, opt *AddOpt) error {
+	if opt == nil {
+		opt = &AddOpt{}
+	}
+	if !opt.Force {
+		if _, err := Lookup(toolchainPath); !os.IsNotExist(err) {
+			return fmt.Errorf("a toolchain already exists at toolchain path %q", toolchainPath)
+		}
 	}
 
 	absDir, err := filepath.Abs(dir)
@@ -28,5 +38,16 @@ func Add(dir, toolchainPath string) error {
 		return err
 	}
 
-	return os.Symlink(absDir, targetDir)
+	if !opt.Force {
+		return os.Symlink(absDir, targetDir)
+	}
+	// Force install the toolchain by removing the directory if
+	// the symlink fails, and then try the symlink again.
+	if err := os.Symlink(absDir, targetDir); err != nil {
+		if err := os.RemoveAll(targetDir); err != nil {
+			return err
+		}
+		return os.Symlink(absDir, targetDir)
+	}
+	return nil
 }
