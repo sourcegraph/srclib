71e8a54f91f6c2e4c10c7cef9e44f2675a7cf3b0
Quinn Slack <qslack@qslack.com>
2015-04-20 21:44:10 -0700

    generate RepoSourceUnit from protobufs

--- unit/repo_source_unit.go
+++ unit/repo_source_unit.go
@@ -2,21 +2,6 @@ package unit
 
 import "encoding/json"
 
-// A RepoSourceUnit is the "concrete" form of SourceUnit that includes
-// information about which repository (and commit) the source unit exists in. In
-// general, type SourceUnit is used during analysis of a single source unit and
-// type RepoSourceUnit is used afterwards (either in cross-source-unit analysis,
-// such as cross-reference resolution, or in after-the-fact DB/API queries).
-type RepoSourceUnit struct {
-	Repo     string `json:",omitempty"`
-	CommitID string `json:",omitempty"`
-	UnitType string `json:",omitempty"`
-	Unit     string `json:",omitempty"`
-
-	// Data is the JSON of the underlying SourceUnit.
-	Data json.RawMessage
-}
-
 // NewRepoSourceUnit creates an equivalent RepoSourceUnit from a
 // SourceUnit.
 //
