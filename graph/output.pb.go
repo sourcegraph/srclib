769a39ea580c95d93a92f92070dee2bcde8214dd
Quinn Slack <qslack@qslack.com>
2015-05-08 00:45:19 -0700

    fix Ann protobuf marshaling

--- graph/output.pb.go
+++ graph/output.pb.go
@@ -7,18 +7,16 @@ package graph
 import proto "github.com/gogo/protobuf/proto"
 
 // discarding unused import gogoproto "github.com/gogo/protobuf/gogoproto/gogo.pb"
-
-
 import ann "sourcegraph.com/sourcegraph/srclib/ann"
 
 // Reference imports to suppress errors if they are not otherwise used.
 var _ = proto.Marshal
 
 type Output struct {
-	Defs []*Def                                        `protobuf:"bytes,1,rep,name=defs" json:"Defs,omitempty"`
-	Refs []*Ref                                        `protobuf:"bytes,2,rep,name=refs" json:"Refs,omitempty"`
-	Docs []*Doc                                        `protobuf:"bytes,3,rep,name=docs" json:"Docs,omitempty"`
-	Anns []*ann.Ann `protobuf:"bytes,4,rep,name=anns,customtype=sourcegraph.com/sourcegraph/srclib/ann.Ann" json:"Anns,omitempty"`
+	Defs []*Def     `protobuf:"bytes,1,rep,name=defs" json:"Defs,omitempty"`
+	Refs []*Ref     `protobuf:"bytes,2,rep,name=refs" json:"Refs,omitempty"`
+	Docs []*Doc     `protobuf:"bytes,3,rep,name=docs" json:"Docs,omitempty"`
+	Anns []*ann.Ann `protobuf:"bytes,4,rep,name=anns" json:"Anns,omitempty"`
 }
 
 func (m *Output) Reset()         { *m = Output{} }
