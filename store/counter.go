25836da88fcc5daa575448ae884284a719257f5d
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2015-11-13 19:44:35 -0700

    store: fix counter (pointers must be the same)

--- store/counter.go
+++ store/counter.go
@@ -4,20 +4,20 @@ import "sync/atomic"
 
 // counter is a simple thread-safe integer.
 type counter struct {
-	count int64
+	count *int64
 }
 
 // increment increments the counter by one.
 func (c counter) increment() {
-	atomic.AddInt64(&c.count, 1)
+	atomic.AddInt64(c.count, 1)
 }
 
 // get returns the counter's current value.
 func (c *counter) get() int {
-	return int(atomic.LoadInt64(&c.count))
+	return int(atomic.LoadInt64(c.count))
 }
 
 // set sets the counter value.
 func (c *counter) set(i int) {
-	atomic.StoreInt64(&c.count, int64(i))
+	atomic.StoreInt64(c.count, int64(i))
 }
