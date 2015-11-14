767b8b3247fd9e6832438f9e8d11076f5066a168
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2015-11-13 19:09:24 -0700

    store: use atomic counters instead of mutex for test global counters

--- store/counter.go
+++ store/counter.go
@@ -1,31 +1,23 @@
 package store
 
-import "sync"
+import "sync/atomic"
 
 // counter is a simple thread-safe integer.
 type counter struct {
-	sync.RWMutex
-	count int
+	count int64
 }
 
 // increment increments the counter by one.
-func (c *counter) increment() {
-	c.Lock()
-	c.count++
-	c.Unlock()
+func (c counter) increment() {
+	atomic.AddInt64(&c.count, 1)
 }
 
 // get returns the counter's current value.
 func (c *counter) get() int {
-	c.RLock()
-	v := c.count
-	c.RUnlock()
-	return v
+	return int(atomic.LoadInt64(&c.count))
 }
 
 // set sets the counter value.
 func (c *counter) set(i int) {
-	c.Lock()
-	c.count = i
-	c.Unlock()
+	atomic.StoreInt64(&c.count, int64(i))
 }
