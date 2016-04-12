267c4e78d9bc347f1638a1c68085f54167ae7d23
Beyang Liu <beyang@sourcegraph.com>
2016-04-11 22:44:33 -0700

    json formatting for Coverage

--- cvg/coverage.go
+++ cvg/coverage.go
@@ -1,10 +1,12 @@
 package cvg
 
+import "encoding/json"
+
 type Coverage struct {
 	FileScore      float64  // % files successfully processed
 	RefScore       float64  // % internal refs that resolve to a def
 	TokDensity     float64  // average number of refs/defs per LoC
-	UncoveredFiles []string // files for which srclib data was not successfully generated (best-effort guess)
+	UncoveredFiles []string `json:",omitempty"` // files for which srclib data was not successfully generated (best-effort guess)
 }
 
 func (c *Coverage) FileScorePass() bool  { return c.FileScore > 0.8 }
